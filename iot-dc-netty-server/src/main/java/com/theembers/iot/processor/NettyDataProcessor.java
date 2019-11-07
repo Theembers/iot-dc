622cd9a92efe902dacb3a5ea5d5e23719a6d2b1d
Theembers <unusual7@163.com>
2019-11-07 14:46:08 +0800

    remove old modules

--- iot-dc-netty-server/src/main/java/com/theembers/iot/processor/NettyDataProcessor.java
+++ iot-dc-netty-server/src/main/java/com/theembers/iot/processor/NettyDataProcessor.java
@@ -1,12 +1,17 @@
 package com.theembers.iot.processor;
 
+import com.theembers.iot.netty.RTUPortListener;
+import com.theembers.iot.redis.IoTService;
 import com.theembers.iot.server.DataProcessor;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import com.theembers.iot.config.NettyConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
 
 /**
  * @author TheEmbers Guo
@@ -14,13 +19,28 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class NettyDataProcessor implements DataProcessor<NettyConfig> {
-    private static final Logger logger = LoggerFactory.getLogger(NettyDataProcessor.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(NettyDataProcessor.class);
     private static EventLoopGroup bossGroup = new NioEventLoopGroup();
     private static EventLoopGroup workerGroup = new NioEventLoopGroup();
 
+    @Autowired
+    private IoTService ioTService;
 
     @Override
     public void executor(NettyConfig nettyConfig) {
-        logger.debug("NettyDataProcessor:executor...");
+        LOGGER.info("Netty Start..");
+        Integer port = nettyConfig.getPort();
+        if (port == null) {
+            LOGGER.warn("port is empty.");
+            return;
+        }
+        // 获取配置
+        try {
+            ioTService.loadIotMapper2Global(InetAddress.getLocalHost().getHostAddress(), port);
+            new RTUPortListener(port, bossGroup, workerGroup);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
